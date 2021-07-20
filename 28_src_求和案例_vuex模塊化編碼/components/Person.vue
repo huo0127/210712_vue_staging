d6392c3abe4d1e93815a157b1cbd419be19eb81f
Leo huo <reason0127301@gmail.com>
2021-07-20 08:45:47 +0800

    210719 vue模塊化

---
+++ 28_src_求和案例_vuex模塊化編碼/components/Person.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <h1>人員列表</h1>
+    <h3 style="color:red">Count組件求和為{{ sum }}</h3>
+    <h3>列表中第一個人的名字是: {{ firstPersonName }}</h3>
+    <input type="text" placeholder="請輸入名字" v-model="name" />
+    <button @click="add">添加</button>
+    <button @click="addWang">添加一個王姓的人</button>
+    <button @click="addPersonServer">添加名字很酷的人</button>
+    <ul>
+      <li v-for="p in personList" :key="p.id">{{ p.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { nanoid } from "nanoid";
+export default {
+  name: "Person",
+  data() {
+    return {
+      name: "",
+    };
+  },
+  methods: {
+    add() {
+      const personObj = { id: nanoid(), name: this.name };
+      this.$store.commit("personAbout/ADD_PERSON", personObj);
+      this.name = "";
+    },
+    addWang() {
+      const personObj = { id: nanoid(), name: this.name };
+      this.$store.dispatch("personAbout/addPersonWang", personObj);
+      this.name = "";
+    },
+    addPersonServer() {
+      this.$store.dispatch("personAbout/addPersonServer");
+    },
+  },
+  computed: {
+    personList() {
+      return this.$store.state.personAbout.personList;
+    },
+    sum() {
+      return this.$store.state.countAbout.sum;
+    },
+    firstPersonName() {
+      return this.$store.getters["personAbout/firstPersonName"];
+    },
+  },
+};
+</script>
+
+<style></style>
